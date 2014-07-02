fe4af9551569f00ce4baf4aca94258b8ce0f4f94
ELAPAKAHARI <elapakahari.1309.com>
2014-07-02 14:18:59 +0530

    Eliminated header files from the LinearRegression.java

--- LinearRegression1.java
+++ LinearRegression1.java
@@ -1,8 +1,8 @@
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+//import java.io.BufferedReader;
+//import java.io.DataInputStream;
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
 class LinearRegression1{
 public static void main(String args[])throws IOException,Exception{
 int MAXN = 1000;
