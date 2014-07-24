f6629dfa654ac990191cdd4929d61decc8597e70
ELAPAKAHARI <elapakahari.1309@gmail.com>
2014-07-24 20:26:10 +0530

    adding helloworld to TERA

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+//Everyone's favourite first Java program
+public class HelloWorld
+{
+//The main entry point
+public static void main(String[] args)
+ {
+  //Declare the output point
+String output ="Hello World";
+//Print a String to the screen
+System.out.println("Hello World");
+ }
+}
+
+
+
+
+
+/Users/rtholmes/dev/git-repositories-for-speculation/git/.git
